89e044f7677b7f8b699b2f49d0eaf842c1c90e04
NLSanyu <lydiansanyu@gmail.com>
2020-08-19 16:16:27 +0300

    Fetch playlists and videos

--- fetch_data.py
+++ fetch_data.py
@@ -4,10 +4,29 @@ from googleapiclient.discovery import build
 api_key = config('API_KEY')
 service = build('youtube', 'v3', developerKey=api_key)
 
-request = service.channels().list(
-    part='statistics',
+channels_request = service.channels().list(
+    part='id, contentDetails',
     forUsername='ntvuganda'
 )
 
-response = request.execute()
-print(response)
+channels_response = channels_request.execute()
+for channel in channels_response['items']:
+    uploads_list_id = channel['contentDetails']['relatedPlaylists']['uploads']
+
+    next_page_token = ''
+    while next_page_token is not None:
+
+        playlistitems_response = service.playlistItems().list(
+            playlistId=uploads_list_id,
+            part='snippet',
+            maxResults=50,
+            pageToken=next_page_token
+        ).execute()
+
+        for playlist_item in playlistitems_response['items']:
+            title = playlist_item['snippet']['title']
+            video_id = playlist_item['snippet']['resourceId']['videoId']
+
+            print(f'{title}, {video_id}')
+
+        next_page_token = playlistitems_response['nextPageToken']
