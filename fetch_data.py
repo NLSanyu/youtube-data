4bc700534ed257754b0de66a1662163a1e908d63
NLSanyu <lydiansanyu@gmail.com>
2020-08-20 11:28:06 +0300

    Add code for fetching captions

--- fetch_data.py
+++ fetch_data.py
@@ -9,13 +9,14 @@ channels_request = service.channels().list(
     forUsername='ntvuganda'
 )
 
+video_ids = []
+
 channels_response = channels_request.execute()
 for channel in channels_response['items']:
     uploads_list_id = channel['contentDetails']['relatedPlaylists']['uploads']
 
-    next_page_token = ''
-    while next_page_token is not None:
-
+    next_page_token = None
+    while next_page_token != '':
         playlistitems_response = service.playlistItems().list(
             playlistId=uploads_list_id,
             part='snippet',
@@ -26,7 +27,24 @@ for channel in channels_response['items']:
         for playlist_item in playlistitems_response['items']:
             title = playlist_item['snippet']['title']
             video_id = playlist_item['snippet']['resourceId']['videoId']
-
             print(f'{title}, {video_id}')
+            video_ids.append({'title': title, 'video_id': video_id})
 
-        next_page_token = playlistitems_response['nextPageToken']
+        # next_page_token = playlistitems_response['nextPageToken']
+        next_page_token = ''   # replace with above line after testing
+
+sample_video_id = video_ids[0]['video_id']
+captions_request = service.captions().list(
+    part='id, snippet',
+    videoId=sample_video_id
+)
+captions_response = captions_request.execute()
+print(captions_response)
+
+# for caption in captions_response['items']:
+#     caption_id = caption['id']
+#     caption_download = service.captions().download(
+#         id=caption_id
+#     ).execute()
+
+#     print(caption_download)
