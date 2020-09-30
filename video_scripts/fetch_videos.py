57e7a8262f78ed637500add98b558aa8a9f67433
NLSanyu <lydiansanyu@gmail.com>
2020-09-30 15:29:28 +0300

    Generate transcripts

--- video_scripts/fetch_videos.py
+++ video_scripts/fetch_videos.py
@@ -1,5 +1,6 @@
 from decouple import config
 from googleapiclient.discovery import build
+import json
 
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
@@ -11,7 +12,7 @@ channels_request = service.channels().list(
     forUsername='ntvuganda'
 )
 
-video_ids = []
+video_list = []
 
 channels_response = channels_request.execute()
 for channel in channels_response['items']:
@@ -30,7 +31,10 @@ for channel in channels_response['items']:
             title = playlist_item['snippet']['title']
             video_id = playlist_item['snippet']['resourceId']['videoId']
             print(f'{title}, {video_id}')
-            video_ids.append({'title': title, 'video_id': video_id})
+            video_list.append({'title': title, 'video_id': video_id})
 
         # next_page_token = playlistitems_response.get('nextPageToken')
-        next_page_token = ''   # replace with above line after testing
+        next_page_token = None   # replace with above line after testing
+
+with open("data/videos.json", "w") as outfile:
+    json.dump(video_list, outfile)
---
+++ video_scripts/generate_transcripts.py
@@ -0,0 +1,26 @@
+import json
+# import fetch_videos
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def transcribe(video_ids):
+    transcript_list = []
+    for video in videos:
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video['video_id'])
+            transcript_list.append(transcript)
+            print(transcript)
+        except RuntimeError as error:
+            print(error)
+        finally:
+            print('Transcript error')
+
+    with open("data/transcripts.json", "w") as outfile:
+        json.dump(transcript_list, outfile)
+
+
+if __name__ == '__main__':
+    # fetch_videos
+    with open("data/videos.json", "r") as read_file:
+        videos = json.load(read_file)
+    transcribe(videos)
