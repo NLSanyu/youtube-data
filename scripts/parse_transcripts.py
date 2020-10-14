ff4c718e0721c41793a2a91ed68b631e912e0539
NLSanyu <lydiansanyu@gmail.com>
2020-10-14 12:29:48 +0300

    Save parsed transcripts to file

--- scripts/parse_transcripts.py
+++ scripts/parse_transcripts.py
@@ -7,16 +7,15 @@ def parse_transcripts(transcripts):
         and saves them to a txt file
         named transcripts_text.txt
     """
-    
+    all_text_list = []
     for transcript in transcripts:
-        text = ''
+        text_list = [f"Title: {transcript[-1]['title']}"]
         for part in transcript:
-            
-            print(transcript['text'])
-            text = text + "\n" + transcript['text']
-    print(text)
-
-    save_text_to_file(text)
+            text_list.append(part.get('text', ' '))
+            text = "\n".join(text_list)
+        all_text_list.append(text)
+    all_text = "\n".join(all_text_list)
+    save_text_to_file(all_text)
     
 def save_text_to_file(text):
     f = open("data/transcripts_text.txt","a")
